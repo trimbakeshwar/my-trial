3df9e62f8860546b1cb7bdbf2293b015652d72a7
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-10 12:56:30 +0530

    initial commit

---
+++ Gambler.java
@@ -0,0 +1,40 @@
+package set4;
+
+import java.util.Scanner;
+
+public class Gambler { 
+
+    public static void main(String[] args) {
+    	Scanner sc=new Scanner(System.in);
+    	System.out.println("stack is");
+    	
+        int stake  = sc.nextInt() ; 
+        System.out.println("goal is");// gambler's stating bankroll
+        int goal   = sc.nextInt() ;
+        System.out.println("rotation");// gambler's desired bankroll
+        int trials = sc.nextInt() ;    // number of trial
+        //s to perform
+
+        int bets = 0;        // total number of bets made
+        int wins = 0;    
+        
+        for(int i=0;i<trials;i++)
+        {
+        	int cash=stake;
+        	
+        	while(cash>0 && cash<goal)
+        	{
+        		bets++;
+        		if(Math.random()<0.5)
+        			cash++;
+        		else
+        			cash--;
+        	}
+        	if(cash==goal)
+        		wins++;
+        }
+        System.out.println(wins+"wins of "+trials);
+        System.out.println("parcentage of game own"+100.0*wins/trials);
+        System.out.println("avg"+1.0*bets/trials);
+    }
+}
